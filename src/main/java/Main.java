32aac4ccc05eb9d78d67fd9f14e219d276cf25e2
shubleru <shubleru@gmail.com>
2021-06-21 11:16:46 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame pagina = new JFrame();
+        pagina.setSize(300, 200);
+        pagina.setLocationRelativeTo(null);
+        pagina.setVisible(true);
+        pagina.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
